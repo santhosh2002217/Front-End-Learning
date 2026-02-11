d7c660fb5320572a8fca9eb8a79548bfc2c05842
santhosh2002217 <santhoshraju1556@gmail.com>
2026-02-11 18:25:19 +0530

    adding files

---
+++ JavaScript/regular expressions/regexp.js
@@ -0,0 +1,17 @@
+// let myemail = "0abcD"
+// let emailpattern = /[a-z]/
+
+// let result = emailpattern.test(myemail)
+// console.log(result);
+
+// let myemail = "ABCDefg"
+// let emailpattern = /[A-Z]/
+
+// let result = emailpattern.test(myemail)
+// console.log(result);
+
+// let myemail = "1ABCde"
+// let emailpattern = /[0-9]/
+
+// let result = emailpattern.test(myemail)
+// console.log(result);
